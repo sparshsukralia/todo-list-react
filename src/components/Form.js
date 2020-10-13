be8eb3a27159b130f6738c49190d27a829eebddb
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-13 16:54:47 +0545

    inputTextHandler

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,16 @@ import React from 'react';
 import "./Form.css"
 
 const Form = () => {
+
+  const inputTextHandler = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="form-container">
       <h3>To-Dos</h3>
       <form className="form">
-        <input type="text" className="todo-input" placeholder="New Todo..."/>
+        <input onChange={inputTextHandler} type="text" className="todo-input" placeholder="New Todo..."/>
         <button className="todo-button"><h2>Add</h2></button>
       </form>
     </div>
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div className="list-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
+}
+
+export default TodoList;
