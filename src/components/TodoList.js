efd4cf344333dc26969107454980a01c8f7859f2
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-14 22:30:27 +0545

    overflow scroll

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Todo from "./Todo";
+import "./TodoList.css";
 
 const TodoList = ({ todo }) => {
   return (
     <div className="list-container">
       <ul className="todo-list">
+        <Todo />
+        <Todo />
         <Todo />
       </ul>
     </div>
